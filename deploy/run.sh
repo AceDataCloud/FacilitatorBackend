bdce6d5cfb05dfc198b5d788817b2fd9857c3494
Germey <cqc@cuiqingcai.com>
2025-11-02 14:37:18 +0800

    fix deploy

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,5 +1,2 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
 cat deploy/production/deployment.yaml | sed 's/\${TAG}/'"${BUILD_NUMBER:-latest}"'/g' | kubectl apply -f - || true
 cat deploy/production/service.yaml | kubectl apply -f - || true
